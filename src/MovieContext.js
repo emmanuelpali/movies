7543dd21cd902c697fa34ce516e98bb389b5b1db
emmanuelpali <palinendir@gmail.com>
2023-05-15 08:18:25 -0600

    updating api

--- src/MovieContext.js
+++ src/MovieContext.js
@@ -7,22 +7,19 @@ const MovieContextProvider = ({ children }) => {
 
     const apiKey = process.env.REACT_APP_RapidAPI;
     const options = {
-        method: 'GET',
-        url: 'https://moviesdatabase.p.rapidapi.com/titles',
-        params: {
-          list: 'most_pop_movies',
-          limit: '20'
-        },
-        headers: {
-          'X-RapidAPI-Key': `${apiKey}`,
-          'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
-        }
-      };
+      method: 'GET',
+      url: 'https://moviesdatabase.p.rapidapi.com/titles',
+      headers: {
+        'X-RapidAPI-Key': `${apiKey}`,
+        'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+      }
+    };
     const getMovies = async () => {
         
         try{
              const response = await axios.request(options)
              setMovies(response.data.results)
+             console.log(movies);
              }
             catch(err) {
              console.log(err);
