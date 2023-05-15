7fe893a1f0165e11a8da0f5273515cb912085ca4
emmanuelpali <palinendir@gmail.com>
2023-05-15 08:46:26 -0600

    displaying movie name

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -8,6 +8,7 @@ export default function Movie({ movie }) {
         <div className="image_container mx-auto">
         {movie.primaryImage ? <img className='card-img-top' src={movie.primaryImage.url} alt={movie.titleText.text} />: movie.titleText.text}
         </div>
+        {movie.originalTitleText && <p className='text-center mt-2'>{movie.originalTitleText.text}</p>}
         {movie.releaseYear && <p className='text-center mt-2'>Release Year: <em>{movie.releaseYear.year}</em></p>}
         <Link className='text-decoration-none' to={`/movie/${movie.id}`}>
         <span className='btn d-block col-5 mx-auto my-3'>Details</span>
