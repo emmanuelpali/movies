2bd1c2615ebecc3c3b23991bfe06ae931ca14aa2
emmanuelpali <palinendir@gmail.com>
2023-06-16 16:54:24 -0600

    changing button styles

--- src/components/Reveal.js
+++ src/components/Reveal.js
@@ -12,7 +12,7 @@ const Reveal = ({ children}) => {
                 hidden: { opacity: 0, y: 75},
                 visible: { opacity: 1, y: 0}
             }}
-            transition={{ duration: 0.5, delay: 0.1}}
+            transition={{ duration: 0.5}}
             >
                 {children}
             </motion.div>
