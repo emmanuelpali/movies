1da81647013cbb7c860dbf6f05292e55da532070
emmanuelpali <palinendir@gmail.com>
2023-06-16 11:23:41 -0600

    adding display on scroll

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import Reveal from './Reveal';
 
 export default function Movie({ movie }) {
   return (
     <>
-      <div className='my_card large-shadow card-dark align-center col-md-4 my-5 mx-5 p-5'>
+      <Reveal >
         <div className="image_container mx-auto">
         {movie.primaryImage ? <img className='card-img-top' src={movie.primaryImage.url} alt={movie.titleText.text} />: movie.titleText.text}
         </div>
@@ -13,7 +14,7 @@ export default function Movie({ movie }) {
         <Link className='text-decoration-none' to={`/movie/${movie.id}`}>
         <span className='btn d-block col-5 mx-auto my-3'>Details</span>
         </Link>        
-      </div>
+      </Reveal>
     </>
   )
 }
---
+++ src/components/Reveal.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useRef } from 'react';
+import { motion, useInView } from "framer-motion"
+
+const Reveal = ({ children}) => {
+    const ref = useRef(null);
+    const isInView = useInView(ref, {once: true})
+  return (
+        <motion.div className='my_card large-shadow card-dark align-center col-md-4 my-5 mx-5 p-5'
+         ref={ref} initial='hidden' animate={isInView ? "visible" : "hidden"}
+        variants={
+            {
+                hidden: { opacity: 0, y: 75},
+                visible: { opacity: 1, y: 0}
+            }}
+            transition={{ duration: 0.5, delay: 0.1}}
+            tra
+            >
+                {children}
+            </motion.div>
+
+  )
+}
+
+export default Reveal
